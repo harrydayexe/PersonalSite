1b39b20e59521cb24048d184cb7db7713afa095d
Harry Day <contact@harryday.xyz>
2026-01-05 23:17:42 +0000

    chore: update dependency to get logging

--- go.mod
+++ go.mod
@@ -2,6 +2,6 @@ module github.com/harrydayexe/PersonalSite
 
 go 1.25.4
 
-require github.com/harrydayexe/GoWebUtilities v1.2.0
+require github.com/harrydayexe/GoWebUtilities v1.3.0
 
 require github.com/caarlos0/env/v11 v11.3.1 // indirect
--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"embed"
 	"log"
-	"log/slog"
 	"net/http"
 
 	"github.com/harrydayexe/GoWebUtilities/server"
@@ -17,35 +16,10 @@ var staticFiles embed.FS
 func main() {
 	ctx := context.Background()
 
-	// setDefaultLogger(serverCfg)
-	logger := slog.Default()
-	logger.Debug("Default logger configured")
-
 	mux := http.NewServeMux()
 	staticcontent.AddStaticRoutes(mux, staticFiles)
-	logger.Debug("Static routes added to mux")
 
 	if err := server.Run(ctx, mux); err != nil {
 		log.Fatal(err)
 	}
 }
-
-// // setDefaultLogger sets the default slog logger to be used in the application
-// func setDefaultLogger(cfg config.ServerConfig) {
-// 	var logger *slog.Logger
-// 	var handlerOptions slog.HandlerOptions
-//
-// 	if cfg.VerboseMode {
-// 		handlerOptions = slog.HandlerOptions{Level: slog.LevelDebug}
-// 	} else {
-// 		handlerOptions = slog.HandlerOptions{Level: slog.LevelInfo}
-// 	}
-//
-// 	if cfg.Environment == config.Local {
-// 		logger = slog.New(slog.NewTextHandler(os.Stdout, &handlerOptions))
-// 	} else {
-// 		logger = slog.New(slog.NewJSONHandler(os.Stdout, &handlerOptions))
-// 	}
-//
-// 	slog.SetDefault(logger)
-// }
